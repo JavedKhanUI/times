da4885d0092f05f5aa31eab0b86f4539d73d7790
Javed Khan <zkzaved@gmail.com>
2023-11-10 02:39:26 +0530

    Brand Slider

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 var slider = tns({
-    container: '.my-slider',
+    container: '.case-slider',
     items: 2,
     loop: true,
     autoplay: true,
@@ -56,8 +56,6 @@ var slider = tns({
     controlsText: ['<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>', '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>'],
     responsive: {
         640: {
-        //   edgePadding: 20,
-        //   gutter: 20,
           items: 2
         },
         700: {
@@ -66,6 +64,35 @@ var slider = tns({
         },
         900: {
           items: 4
+        }
+      }
+  });
+
+//  Brand Slider
+
+var slider = tns({
+    container: '.brand-slider',
+    items: 1,
+    swipeAngle: false,
+    loop: true,
+    autoplay: true,
+    speed: 100,
+    gutter: 20,
+    nav: false,
+    autoplayButtonOutput: false,
+    controlsText: ['<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>', '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>'],
+    responsive: {
+        640: {
+          items: 2
+        },
+        700: {
+          gutter: 30,
+          items: 2
+        },
+        900: {
+          items: 4,
+          gutter: 40,
+          fixedWidth: 300,
         }
       }
   });
